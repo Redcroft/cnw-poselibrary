fc9b3d582affc73f23f0258a2d52a3cebedeec01
Richard DW Redcroft <Richard@redcroft.tech>
2022-02-01 12:25:31 +0000

    Library now displays animated thumbnails

--- python3.7libs/cnwpose/capture.py
+++ python3.7libs/cnwpose/capture.py
@@ -55,7 +55,7 @@ class UI(QtWidgets.QWidget):
         self.btn_cap_pose.clicked.connect(self._capturePose)
         self.btn_cap_pose.setFixedWidth(hou.ui.scaledSize(147))
         self.btn_screenshot = QtWidgets.QPushButton('Screenshot')
-        self.btn_screenshot.clicked.connect(self._captureStill)
+        self.btn_screenshot.clicked.connect(self._captureSequence)
         btn_layout = QtWidgets.QHBoxLayout()
         btn_layout.addWidget(self.btn_cap_clip)
         btn_layout.addWidget(self.btn_cap_pose)
@@ -67,7 +67,6 @@ class UI(QtWidgets.QWidget):
         main_layout.addWidget(self.te_debug)
         # main_layout.addStretch()
 
-
     def _captureClip(self):
         '''Capture Animation clip from the selected channels.
 The time range is offset to start at frame 0, rather than when it currently starts'''
@@ -77,7 +76,8 @@ The time range is offset to start at frame 0, rather than when it currently star
         start_time = hou.frameToTime(frame_range[0])
         sel_channels = self._selectChannels()
         if len(sel_channels) == 0:
-            utils.warningDialog('No channels are available in the Channel List')
+            utils.warningDialog(
+                'No channels are available in the Channel List')
             return False
         anim_dict = {}
         for p in sel_channels:
@@ -110,7 +110,8 @@ The time range is offset to start at frame 0, rather than when it currently star
         '''Capture a Pose from the selected controls in the channel list. The stored frame starts from zero'''
         sel_channels = self._selectChannels()
         if len(sel_channels) == 0:
-            utils.warningDialog('No channels are avaliable in the Channel List')
+            utils.warningDialog(
+                'No channels are avaliable in the Channel List')
             return False
         anim_dict = {}
         for p in sel_channels:
@@ -132,7 +133,7 @@ The time range is offset to start at frame 0, rather than when it currently star
         ''' Return a tuple of all the currently selected channels in the
         Channel List'''
         selection = hou.playbar.channelList().selected()
-        if len(selection)==0:
+        if len(selection) == 0:
             selection = hou.playbar.channelList().parms()
         return selection
 
@@ -159,35 +160,64 @@ The time range is offset to start at frame 0, rather than when it currently star
             utils.warningDialog(f"Unable to read file.\nError: {e}")
             return False
 
-    def _captureStill(self):
-        # GET VIEWPORT CAMERA PATH
+    def _captureSequence(self):
+        cur_frame = hou.frame()
+        object = hou.playbar.channelList().parms()[0].node()
+        if object is not None:
+            while type(object) is not hou.ObjNode:
+                object = object.parent()
+        else:
+            object = False
+        img_filenames = []
+        for i in range(48):
+            hou.setFrame(i)
+            filename = hou.expandString("$HOME/temp/clip.$F4.jpg")
+            img_filenames.append(filename)
+            self._captureThumbnail(i, filename, object)
+        hou.setFrame(cur_frame)
+        self._convertImagesToGif(img_filenames)
+
+    def _convertImagesToGif(self, filename_list):
+        gif = []
+        base_dir = os.path.dirname(filename_list[0])
+        filename = os.path.basename(filename_list[0]).split(".")[0] + ".gif"
+        filename = os.path.join(base_dir, filename)
+        duration = (1.0/hou.fps())*1000
+        print(duration)
+        print(filename)
+        for i in filename_list:
+            image = Image.open(i)
+            gif.append(image.convert("P", palette=Image.ADAPTIVE))
+            os.remove(i)
+        gif[0].save(filename, save_all=True,
+                    optimize=False, append_images=gif[1:], loop=0, duration=duration)
+
+    def _captureThumbnail(self, frame, filename, object):
         cur_desktop = hou.ui.curDesktop()
         desktop = cur_desktop.name()
         panetab = cur_desktop.paneTabOfType(hou.paneTabType.SceneViewer).name()
-        persp = cur_desktop.paneTabOfType(hou.paneTabType.SceneViewer).curViewport().name()
+        persp = cur_desktop.paneTabOfType(
+            hou.paneTabType.SceneViewer).curViewport().name()
         camera_path = f"{desktop}.{panetab}.world.{persp}"
 
-        # BUILD DEFAULT FILE NAME FROM CURRENT TIME
-        default_filename = "screenshot.jpg"
-
-        # SELECT FILE
-        filename = hou.expandString("$HOME/screenshot.jpg")
-        object = hou.playbar.channelList().parms()[0].node().name()
-
-        # WRITE TO FILE
+        temp = os.path.join(os.path.dirname(filename), "temp.jpg")
         if filename is not None:
-            frame = hou.frame()
-            hou.hscript(f"viewwrite -v {object} -R beauty -f {frame} {frame} {camera_path} {filename}")
+            if object is False:
+                hou.hscript(
+                    f"viewwrite -R beauty -f {frame} {frame} {camera_path} {temp}")
+            else:
+                hou.hscript(
+                    f"viewwrite -v {object} -R beauty -f {frame} {frame} {camera_path} {temp}")
 
         try:
-            img = Image.open(filename)
+            img = Image.open(temp)
             w, h = img.size
             crop = min(w, h)
             img2 = img.crop(((w - crop)//2,
                             (h - crop)//2,
                             (w + crop)//2,
-                            (h + crop)//2)).resize((512,512))
-            filename_crop = hou.expandString("$HOME/screenshot_square.jpg")
+                            (h + crop)//2)).resize((192, 192))
             img2.save(filename)
-        except:
-            print("Failed")
+            os.remove(temp)
+        except Exception as e:
+            utils.warningDialog(f"Unable to save thumbnail\nError: {e}")
--- python3.7libs/cnwpose/library.py
+++ python3.7libs/cnwpose/library.py
@@ -2,7 +2,7 @@ import hou
 import psutil
 
 from PySide2 import QtWidgets
-from PySide2.QtGui import QPixmap
+from PySide2.QtGui import QMovie
 
 from . import plglobals
 
@@ -28,7 +28,8 @@ class UI(QtWidgets.QWidget):
         self.btn_clear = QtWidgets.QPushButton('Clear')
         self.btn_clear.clicked.connect(self._clearLibrary)
         main_layout.addWidget(self.btn_clear)
-        self.lbl_mem = QtWidgets.QLabel(f"{psutil.Process().memory_info().rss / (1024 * 1024)}")
+        self.lbl_mem = QtWidgets.QLabel(
+            f"{psutil.Process().memory_info().rss / (1024 * 1024)}")
         main_layout.addWidget(self.lbl_mem)
         self.grid_layout = QtWidgets.QGridLayout()
         self._refreshLibrary()
@@ -36,24 +37,39 @@ class UI(QtWidgets.QWidget):
         main_layout.addLayout(self.grid_layout)
 
     def _refreshLibrary(self):
-        filename = hou.expandString("$HOME/screenshot.jpg")
+        filename = hou.expandString("$HOME/temp/clip.gif")
         self._clearLibrary()
         for x in range(3):
-            for y in range(3):
-                label = QtWidgets.QLabel(f"{x}, {y}")
-                label.setPixmap(QPixmap(filename).scaled(192, 192))
-                self.grid_layout.addWidget(label,x,y)
-        self.lbl_mem.setText(f"{psutil.Process().memory_info().rss / (1024 * 1024)}")
+            for y in range(4):
+                container = QtWidgets.QVBoxLayout()
+                thumbnail = QtWidgets.QLabel()
+                thumbnail.setMovie(QMovie(filename))
+                thumbnail.movie().start()
+                thumbnail.setScaledContents(True)
+                thumbnail.setFixedSize(192, 192)
+                label = QtWidgets.QLabel('Thumbnail')
+                container.addWidget(thumbnail)
+                container.addWidget(label)
+                self.grid_layout.addLayout(container, x, y)
 
     def _clearLibrary(self):
         rows = self.grid_layout.rowCount()
         cols = self.grid_layout.columnCount()
-        for i in range(10):
-            item = self.grid_layout.itemAt(0)
-            print(f"i = {i}\t{item}")
-            if item is not None:
-                widget = item.widget()
-                if widget is not None:
-                    self.grid_layout.removeWidget(widget)
-                    widget.deleteLater()
-        self.lbl_mem.setText(f"{psutil.Process().memory_info().rss / (1024 * 1024)}")
+        for i in range(rows*cols+1):
+            container = self.grid_layout.itemAt(0)
+            if container is not None:
+                container.setParent(None)
+                count = container.count()
+                for i in range(count):
+                    item = container.itemAt(i)
+                    if item is not None:
+                        widget = item.widget()
+                        if widget is not None:
+                            if widget.movie() is not None:
+                                widget.movie().deleteLater()
+                            if widget.pixmap() is not None:
+                                widget.pixmap().deleteLater()
+                            widget.setParent(None)
+                            widget.deleteLater()
+        self.lbl_mem.setText(
+            f"{psutil.Process().memory_info().rss / (1024 * 1024):.2f} Mb memory used")
